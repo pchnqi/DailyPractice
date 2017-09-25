c1afb574d4171a7ad96d5a5cc452b5ff0de77cb5
=nqi <=nqi@pch.com>
2017-09-25 16:54:51 -0400

    Update findCommon for Algorithm

--- public/alogrithm.php
+++ public/alogrithm.php
@@ -6,11 +6,20 @@ use Source\Algorithm\Algorithm;
 
 $algorithm = new Algorithm();
 
-$size = 10;
-$test_array = $algorithm->randomArray($size, true);
+
+$test_array = $algorithm->randomArray(10, true);
 $after_reverse = $algorithm->reverse($test_array);
 
-writeln(print_array($test_array));
-writeln(print_array($after_reverse));
+writeln('Array1: ' . print_array($test_array));
+writeln('Array2: '. print_array($after_reverse));
+
+$t2 = $algorithm->randomArray(30, true);
+$t3 = $algorithm->randomArray(40, true);
+
+$common = $algorithm->findCommon($t2, $t3);
+
+writeln('Sorted Array A: ' . print_array($t2));
+writeln('Sorted Array B: ' . print_array($t3));
+writeln('Common: ' . print_array($common));
 
 echo 'hello, world';
--- src/Algorithm/Algorithm.php
+++ src/Algorithm/Algorithm.php
@@ -23,12 +23,40 @@ class Algorithm
         return $array;
     }
 
+    public function findCommon ( Array $sortedA, Array $sortedB )
+    {
+        $i = 0;
+        $j = 0;
+        $sizeA = count($sortedA);
+        $sizeB = count($sortedB);
+
+        $common = [];
+
+        while ( $i < $sizeA && $j < $sizeB ) {
+            if ( $sortedA[$i] < $sortedB[$j]) {
+                $i++;
+                continue;
+            }
+
+            if ( $sortedA[$i] > $sortedB[$j]) {
+                $j++;
+                continue;
+            }
+
+            $common[] = $sortedA[$i];
+            $i++;
+            $j++;
+        }
+
+        return array_unique($common);
+    }
+
     public function randomArray ( $size, $isSort = false, $start = 0, $end = 100 )
     {
         $array = [];
         
         while ( $size >= 0) {
-            $array[] = rand($start, $end);
+            $array[] = mt_rand($start, $end);
             $size--;
         }
 
